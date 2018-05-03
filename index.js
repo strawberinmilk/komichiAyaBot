3632b31d62adc78b500d3e99534fc39d52d1df04
rinkei <rinkei.krt6006@gmail.com>
2018-05-03 10:57:44 +0900

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const fs = require("fs");
 
-let keitaiso = fs.readFileSync("./data/keitaiso.txt", "utf8");
-keitaiso = keitaiso.split(",");
+const keitaiso = fs.readFileSync("./data/keitaiso.txt", "utf8").split(",");
 
-function ayayaSay() {
+const ayayaSay = () =>{
     let ayaya = [];
     let serchWord = " ";
-    while (1 === 1) {
+    while (true) {
         let indexofStart = -1;
         let wordNumberList = [];
-        while (1 === 1) {
+        while (true) {
             indexofStart += 1;
             indexofStart = keitaiso.indexOf(serchWord, indexofStart);
             if (indexofStart === -1 || indexofStart === keitaiso.length - 1) {
@@ -29,12 +28,15 @@ function ayayaSay() {
     return ayaya;
 }
 
-function call() {
-    while (1 === 1) {
+const call = () => {
+    while (true) {
         let ayaya = ayayaSay();
         if ( !ayaya.match(/しね$/) && ayaya.length > 25) {
             return ayaya;
             break;
+        }else{
+            ayaya = null
+            continue;
         }
     }
 }
@@ -53,7 +55,8 @@ console.log(num/1000);
 const twitter = require("twitter");
 const cron = require('cron').CronJob;
 require('dotenv').config();
-key = new twitter({
+console.log(process.env.consumer_key)
+const key = new twitter({
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
     access_token_key: process.env.access_token_key,
@@ -61,29 +64,27 @@ key = new twitter({
 });
 
 key.get("account/verify_credentials", function (error, data) {
-    mydata = JSON.stringify(data)
+    let mydata = JSON.stringify(data)
     mydata = JSON.parse(mydata)
     //console.log(mydata)
-
     console.log("認証アカウント")
     console.log("@" + mydata.screen_name)
     console.log(mydata.name)
     console.log("\n")
-
 })
 
-key.stream('user', function (stream) {
-    stream.on('follow', function (data) {
+key.stream('user', stream => {
+    stream.on('follow', data => {
         key.post('friendships/create', { user_id: data.source.id_str });
     });
 });
 
-let cronAyaya = new cron({
+const cronAyaya = new cron({
     cronTime: '0 0,10,20,30,40,50 * * * *',
-    onTick: function () {
-        let postData = call()
+//    cronTime: '* * * * * *',
+    onTick:  () => {
         key.post('statuses/update',
-            { status: postData },
+            { status: call() },
             function (error, tweet, response) {
             })
     },
