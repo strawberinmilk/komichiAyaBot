b2803bc0b99ee6cd231da8bf2f49e2a8d2c3c133
rinkei <rinkei.krt6006@gmail.com>
2018-09-15 00:59:43 +0900

    repairIndent

--- index.js
+++ index.js
@@ -5,42 +5,42 @@ const keitaiso = fs.readFileSync("./data/keitaiso.txt", "utf8").split(",");
 let call = (serchWord)=>{
 let ayayaSay = (serchWord)=>{
 
-return ((serchWord) => {
+  return ((serchWord) => {
     let ayaya = [];
     if (!serchWord) {
-        serchWord = " ";
+      serchWord = " ";
     }
     let noDataFlag = true;
     while (true) {
-        let indexofStart = -1;
-        let wordNumberList = [];
-        while (true) {
-            indexofStart += 1;
-            indexofStart = keitaiso.indexOf(serchWord, indexofStart);
-            if(indexofStart=== -1 && noDataFlag) return ["nothit","ごめんなさい、わからないわ。"]
-            noDataFlag = false
-            if (indexofStart === -1 || indexofStart === keitaiso.length - 1) {
-                indexofStart = null;
-                break;
-            };
-            wordNumberList.push(indexofStart + 1);
+      let indexofStart = -1;
+      let wordNumberList = [];
+      while (true) {
+        indexofStart += 1;
+        indexofStart = keitaiso.indexOf(serchWord, indexofStart);
+        if(indexofStart=== -1 && noDataFlag) return ["nothit","ごめんなさい、わからないわ。"]
+        noDataFlag = false
+        if (indexofStart === -1 || indexofStart === keitaiso.length - 1) {
+          indexofStart = null;
+          break;
         };
-        let wordNumber = wordNumberList[Math.floor(Math.random() * wordNumberList.length)];
-        indexofStart = null;
-        wordNumberList = null;
-        if (keitaiso[wordNumber] === " ") {
-            break;
-        } else {
-            serchWord = keitaiso[wordNumber];
-            ayaya.push(keitaiso[wordNumber]);
-        }
-        wordNumber = null
+        wordNumberList.push(indexofStart + 1);
+      };
+      let wordNumber = wordNumberList[Math.floor(Math.random() * wordNumberList.length)];
+      indexofStart = null;
+      wordNumberList = null;
+      if (keitaiso[wordNumber] === " ") {
+        break;
+      } else {
+        serchWord = keitaiso[wordNumber];
+        ayaya.push(keitaiso[wordNumber]);
+      }
+      wordNumber = null
     }
     serchWord = null;
     ayaya = ayaya.join("");
     return [null,ayaya];
     ayaya = null;
-})(serchWord)
+  })(serchWord)
 }
 
 //let call = (serchWord) => {
@@ -52,19 +52,19 @@ return ans
 
 
 /*
-     while (true) {
-        let ayaya = ayayaSay(serchWord);
-        if(ayaya[0]) return ayaya[1]
-        ayaya = ayaya[1]
-        if (!ayaya.match(/しね$/) && ayaya.length > 25) {
-            return ayaya;
-            ayaya = null;
-            break;
-        } else {
-            ayaya = null;
-            continue;
-        }
+  while (true) {
+    let ayaya = ayayaSay(serchWord);
+    if(ayaya[0]) return ayaya[1]
+    ayaya = ayaya[1]
+    if (!ayaya.match(/しね$/) && ayaya.length > 25) {
+      return ayaya;
+      ayaya = null;
+      break;
+    } else {
+      ayaya = null;
+      continue;
     }
+  }
 */
 }
 console.log(call("綾"))
@@ -72,32 +72,31 @@ console.log(call("綾"))
 /*
 let num = 0;
 for(let i=0;i<1000;i++){
-    let tmp = call("綾");
-    num += tmp.length
-    console.log(`${tmp}${i}`)
-    console.log(process.memoryUsage().heapUsed + "/" +process.memoryUsage().heapTotal)
+  let tmp = call("綾");
+  num += tmp.length
+  console.log(`${tmp}${i}`)
+  console.log(process.memoryUsage().heapUsed + "/" +process.memoryUsage().heapTotal)
 }
 console.log(num/1000);
 */
 
 /*
 setInterval(()=>{
-    console.log(new Date + process.memoryUsage().heapUsed + "/" +process.memoryUsage().heapTotal)
+  console.log(new Date + process.memoryUsage().heapUsed + "/" +process.memoryUsage().heapTotal)
 },10000)
 */
 
 function checkMemory() {
 
-    // gc.
-    try {
-        global.gc();
-    } catch (e) {
-        console.log('You have to run this program as `node --expose-gc app_memory_leak.js`');
-    }
-
-    // Check heap memory.
-    const heapUsed = process.memoryUsage().heapUsed;
-    console.log('heapSize: ', heapUsed);
+// gc.
+try {
+    global.gc();
+} catch (e) {
+    console.log('You have to run this program as `node --expose-gc index.js`');
+}
+// Check heap memory.
+const heapUsed = process.memoryUsage().heapUsed;
+console.log('heapSize: ', heapUsed);
 }
 
 setInterval(checkMemory, 10000);
@@ -107,40 +106,30 @@ const twitter = require("twitter");
 const cron = require('cron').CronJob;
 require('dotenv').config();
 const key = new twitter({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token_key: process.env.access_token_key,
-    access_token_secret: process.env.access_token_secret
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
 });
 
 key.get("account/verify_credentials", function (error, data) {
-    let mydata = JSON.stringify(data)
-    mydata = JSON.parse(mydata)
-    //console.log(mydata)
-    console.log("認証アカウント")
-    console.log("@" + mydata.screen_name)
-    console.log(mydata.name)
-    console.log("\n")
-    mydata = null;
+  let mydata = JSON.stringify(data)
+  mydata = JSON.parse(mydata)
+  //console.log(mydata)
+  console.log("認証アカウント")
+  console.log("@" + mydata.screen_name)
+  console.log(mydata.name)
+  console.log("\n")
+  mydata = null;
 })
-/*
-key.stream('user', stream => {
-    stream.on('follow', data => {
-        key.post('friendships/create', { user_id: data.source.id_str });
-    });
-});
-*/
+
 const posttweet = () =>{
-    key.post('statuses/update',
-    { status: call() },
-    (error, tweet, response) => {
-    })
+  key.post('statuses/update',
+  { status: call() },
+  (error, tweet, response) => {
+  })
 }
-/*
-setInterval(()=>{
-    posttweet()
-},600000)
-*/
+
 
 const cronAyaya = new cron({
     cronTime: '0 0,10,20,30,40,50 * * * *',
@@ -153,3 +142,17 @@ const cronAyaya = new cron({
 });
 cronAyaya.start();
 posttweet()
+
+/*
+setInterval(()=>{
+    posttweet()
+},600000)
+*/
+
+/*
+key.stream('user', stream => {
+    stream.on('follow', data => {
+        key.post('friendships/create', { user_id: data.source.id_str });
+    });
+});
+*/
